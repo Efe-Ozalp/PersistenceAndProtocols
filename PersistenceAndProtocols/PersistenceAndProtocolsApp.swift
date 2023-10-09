fda654e2f0da3130b0bedd4fb15abe08d32eb90c
Efe Ozalp <efeozalp0101@gmail.com>
2023-10-09 10:11:43 -0500

    Initial Commit

---
+++ PersistenceAndProtocols/PersistenceAndProtocolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistenceAndProtocolsApp.swift
+//  PersistenceAndProtocols
+//
+//  Created by Efe Ozalp on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PersistenceAndProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersistenceAndProtocolsUITests/PersistenceAndProtocolsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PersistenceAndProtocolsUITestsLaunchTests.swift
+//  PersistenceAndProtocolsUITests
+//
+//  Created by Efe Ozalp on 10/9/23.
+//
+
+import XCTest
+
+final class PersistenceAndProtocolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
